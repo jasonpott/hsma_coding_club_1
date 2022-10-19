5eaaf5393ea9a748d644a6b8c280f6561e8e8bb0
Jason Pott <43917006+jasonpott@users.noreply.github.com>
2022-10-19 18:03:03 +0100

    Narrative list created
    Added narrative - more to come

--- hsma_cc_1.py
+++ hsma_cc_1.py
@@ -123,7 +123,19 @@ while talk_to_security == False:
 #_____,     v        ,_____
 #nil,     Toilet,        nil
 
-
+narrative = {
+    "s1":"You are at the entrance of a dilapidated NHS office block. There is a door to the corridor.There is a security desk in front of you, manned by an agressive looking security guard.", # First time in security
+    "s2":"You have returned to the security desk next to the hospital entrance. The security guard is sat at the desk still watching football on his phone", # Second time in security
+    "c1":"You escape the distain of the scurity guard and enter a dreary corridor. A strong smell of bleach and overcooked peas hangs in the air. There are three doors off the corridor, one has a guardian near by", # First time in corridor
+    "c2":"Back in the corridor, the bleached peas smell has gone, replaced with ",# Second time in corridor
+    "o1":"You enter the office. The walls are gleaming white, clearly the hospital managment company has recently renewed the paint. On entering you receive a warm welcome from your manager, who welcomes you to the team. This is your first day working at St Hoots and Wotsits NHS Trust. Time for induction. Some time later you wake bleary eyed and you manager is still talking, now rather hoarsly. Your manager suggests that it is time to take a break. You are informed there is a delicious Taste the difference tripple chocolate fudge cake in the adjoining staff room and you should join the rest of the team for a slice and a cup of tea. you stand up and look around.", # First time in office
+    "o2":"You enter the office. The Manager looks up saying, 'why are you back? You haven't been gone nearly long enough to have gotten your id badge yet!'.",# second time in office no achievements have tried the sr door
+    "o3":"You enter the office. The manager looks up saying: 'Oh so you managed to run the HR gaunlet, congrats! Your predecessor never managed to return hence why we re-advertised the position.",# Third time in office has HR 
+    "o4":"You enter the office. The manager looks up wide eyed 'Goodness, this has never happened before. You are the promised one who was fortold to be able to obtain an ID badge in one single day.",# fourth time in office has HR has ID badge
+    "h1":"You enter HR. from some where in the mass of office cubicles you can hear a quiet sobbing. A Senior HR specialist is leant up against a desk chewing gum with a glazzed expression. ", # First time in hr
+    "h2":"",# second time in ht
+    "t1":"" # First time in toilet
+}
 #Security
 #You are at the entrance of a dilapidated NHS office block. There is a door to the corridor.
 #There is a security desk in front of you, manned by an agressive looking security guard.
